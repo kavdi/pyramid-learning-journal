67e9960f69d25d13acd7406a9b44707823b74b79
kavdihodgson <kavdyjh@gmail.com>
2017-11-02 16:50:42 -0700

    got functionality working. starting tests.

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -18,30 +18,30 @@ def list_view(request):
 @view_config(route_name='detail', renderer="learning_journal:templates/view_entry.jinja2")
 def detail_view(request):
     posts_id = int(request.matchdict['id'])
-    if posts_id < 0 or posts_id > len(POSTS) - 1:
+    if posts_id < 0 or posts_id > len(POSTS):
         raise HTTPNotFound
-    posts = list(filter(lambda posts: posts['id'] == posts_id, POSTS))[0]
+    post = list(filter(lambda posts: posts['id'] == posts_id, POSTS))[0]
     return {
         'title': 'Blog Post',
-        'posts': POSTS
+        'post': post
     }
 
 
 @view_config(route_name="create", renderer="learning_journal:templates/crea_entry.jinja2")
 def create_view(request):
-    posts_id = list(filter(lambda post: post['id'] == posts_id, POSTS))[0]
     return {
         'title': 'Create Blog Post',
+        'date': datetime.now()
     }
 
 
 @view_config(route_name="update", renderer="learning_journal:templates/edit_entry.jinja2")
 def update_view(request):
     posts_id = int(request.matchdict['id'])
-    if posts_id < 0 or posts_id > len(POSTS) - 1:
+    if posts_id < 0 or posts_id > len(POSTS):
         raise HTTPNotFound
-    posts = list(filter(lambda post: post['id'] == posts_id, POSTS))[0]
+    post = list(filter(lambda post: post['id'] == posts_id, POSTS))[0]
     return {
         'title': 'Edit Post',
-        'posts': POSTS
+        'post': post
     }
