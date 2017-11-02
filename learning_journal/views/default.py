5ad59dbc38e872cd81ea01c48f4bfead191a993b
kavdihodgson <kavdyjh@gmail.com>
2017-11-01 21:47:31 -0700

    its not working right...

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -2,7 +2,7 @@
 from pyramid.view import view_config
 from datetime import datetime
 from pyramid.httpexceptions import HTTPNotFound
-from blog_post import POSTS
+from learning_journal.data.blog_post import POSTS
 
 FMT = '%Y/%m/%d'
 
@@ -17,10 +17,10 @@ def list_view(request):
 
 @view_config(route_name='detail', renderer="learning_journal:templates/view_entry.jinja2")
 def detail_view(request):
-    post_id = int(request.matchdict['id'])
-    if post_id < 0 or post_id > len(POSTS) - 1:
+    posts_id = int(request.matchdict['id'])
+    if posts_id < 0 or posts_id > len(POSTS) - 1:
         raise HTTPNotFound
-    posts = list(filter(lambda posts: posts['id'] == post_id, POSTS))[0]
+    posts = list(filter(lambda posts: posts['id'] == posts_id, POSTS))[0]
     return {
         'title': 'Blog Post',
         'posts': POSTS
@@ -29,7 +29,7 @@ def detail_view(request):
 
 @view_config(route_name="create", renderer="learning_journal:templates/crea_entry.jinja2")
 def create_view(request):
-    post_id = list(filter(lambda post: post['id'] == post_id, POSTS))[0]
+    posts_id = list(filter(lambda post: post['id'] == posts_id, POSTS))[0]
     return {
         'title': 'Create Blog Post',
     }
@@ -37,10 +37,10 @@ def create_view(request):
 
 @view_config(route_name="update", renderer="learning_journal:templates/edit_entry.jinja2")
 def update_view(request):
-    post_id = int(request.matchdict['id'])
-    if post_id < 0 or post_id > len(POSTS) - 1:
+    posts_id = int(request.matchdict['id'])
+    if posts_id < 0 or posts_id > len(POSTS) - 1:
         raise HTTPNotFound
-    posts = list(filter(lambda post: post['id'] == post_id, POSTS))[0]
+    posts = list(filter(lambda post: post['id'] == posts_id, POSTS))[0]
     return {
         'title': 'Edit Post',
         'posts': POSTS
