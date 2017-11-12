39f748bab4c0260858f97b3adfbf32216e56caa4
kavdihodgson <kavdyjh@gmail.com>
2017-11-11 22:34:45 -0800

    fixed tests, having one error in python2.7 that needs to be fixed.

--- learning_journal/tests.py
+++ learning_journal/tests.py
@@ -1,44 +1,43 @@
 """."""
 from pyramid.testing import DummyRequest
 import pytest
-from datetime import datetime
-from bs4 import BeautifulSoup
 
 
-def test_home_list_view_shows_list_of_titles_in_dict():
+@pytest.fixture
+def req():
+    """Create a dummy GET request."""
+    return DummyRequest()
+
+
+def test_home_list_view_shows_list_of_titles_in_dict(req):
     from learning_journal.views.default import list_view
-    req = DummyRequest()
     response = list_view(req)
     assert 'posts' in response
     assert isinstance(response['posts'], list)
 
 
-def test_home_lists_view_includes_title():
+def test_home_lists_view_includes_title(req):
     from learning_journal.views.default import list_view
-    req = DummyRequest()
     response = list_view(req)
     assert 'title' in response['posts'][0]
 
 
-def test_edit_lists_view_includes_title():
+def test_edit_lists_view_includes_title(req):
     from learning_journal.views.default import update_view
-    req = DummyRequest()
     req.matchdict['id'] = 2
     response = update_view(req)
     assert 'title' in response['post']
 
 
-def test_view_includes_post_title():
+def test_view_includes_post_title(req):
     from learning_journal.views.default import detail_view
-    req = DummyRequest()
     req.matchdict['id'] = 3
     response = detail_view(req)
     assert 'Day 3' in response['post']['title']
 
 
-def test_creat_view_includes_post_title():
+def test_creat_view_includes_post_title(req):
     from learning_journal.views.default import create_view
-    req = DummyRequest()
     response = create_view(req)
     assert isinstance(response, dict)
 
@@ -67,15 +66,17 @@ def test_home_route_has_h4_titles(testapp):
 
 def test_detail_view_route_has_text(testapp):
     response = testapp.get("/journal/9")
+    print(response.html)
     assert "cookiecutter... pyramids...deques..." in str(response.html)
 
 
-def test_edit_view_route_has_correct_title(testapp):
+def test_update_view_has_an_submit_button(testapp):
     response = testapp.get("/update/2")
-    assert "Day 2" in str(response.html)
+    assert len(response.html.find_all('button', attrs={"type": "submit"})) == 1
+    assert "Submit" in response.html.find('button', attrs={"type": "submit"})
 
 
-def test_create_view_has_no_current_date(testapp):
+def test_create_view_has_a_submit_button(testapp):
     response = testapp.get("/create")
-    soup = BeautifulSoup(response.text, 'html.parser')
-    assert soup.find('form')
+    assert len(response.html.find_all('button', attrs={"type": "submit"})) == 1
+    assert "Submit" in response.html.find('button', attrs={"type": "submit"})
